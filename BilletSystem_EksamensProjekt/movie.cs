10b0b866d0f1252f77742fd89e78b6bf08fa5bcb
Lasse Henriksen <lass5676@edu.easj.dk>
2018-05-16 02:21:19 +0200

    Så skulle listen af film virke som aftalt

--- BilletSystem_EksamensProjekt/movie.cs
+++ BilletSystem_EksamensProjekt/movie.cs
@@ -1,7 +1,44 @@
-﻿namespace BilletSystem_EksamensProjekt
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BilletSystem_EksamensProjekt.Annotations;
+
+namespace BilletSystem_EksamensProjekt
 {
-    public class movie
+    public class movie : INotifyPropertyChanged
     {
-        
+        private List<movie_showtime> _movies;
+        private movie_showtime _selectedMovie;
+
+        public movie()
+        {
+            _movies = new List<movie_showtime>();
+            _movies.Add(new movie_showtime("Avengers: Infinity War", 1, "Dette er bare en superhelte film", "20/4-18"));
+            _movies.Add(new movie_showtime("Deadpool 2", 2, "Dette er bare en anti-superhelte film", "15/5-18"));
+            _movies.Add(new movie_showtime("Film 3", 3, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "15/5-18"));
+            _selectedMovie = _movies[0];
+        }
+
+        public List<movie_showtime> Movies
+        {
+            get { return _movies; }
+        }
+
+        public movie_showtime SelectedMovie
+        {
+            get { return _selectedMovie; }
+            set
+            {
+                _selectedMovie = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- BilletSystem_EksamensProjekt/movie_showtime.cs
+++ BilletSystem_EksamensProjekt/movie_showtime.cs
@@ -2,6 +2,39 @@
 {
     public class movie_showtime
     {
-        
+        private string _title;
+        private int _sal;
+        private string _description;
+        private string _realese;
+
+
+        public string Title
+        {
+            get { return _title; }
+        }
+
+        public int Sal
+        {
+            get { return _sal; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+        }
+
+        public string Realese
+        {
+            get { return _realese; }
+        }
+
+        public movie_showtime(string Title, int Sal, string description, string realese)
+        {
+            _title = Title;
+            _sal = Sal;
+            _description = description;
+            _realese = realese;
+        }
+
     }
 }
