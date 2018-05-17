3b301e0f8e010b359d33da61fe5b19972dd1f82b
Jacob Larsen <captainpowerturtle@gmail.com>
2018-05-17 13:25:29 +0200

    Tidspunkts Liste

--- BilletSystem_EksamensProjekt/tidspunkt.cs
+++ BilletSystem_EksamensProjekt/tidspunkt.cs
@@ -1,9 +1,29 @@
-﻿using System.Collections.Generic;
-
-namespace BilletSystem_EksamensProjekt
+﻿namespace BilletSystem_EksamensProjekt
 {
-    public class tidspunkt
+    public class Tidspunkt
     {
+        #region Instance Field
 
+        private string _tid;
+
+        #endregion
+
+        #region Constructor
+
+        public Tidspunkt(string tid)
+        {
+            _tid = tid;
+        }
+
+        #endregion
+
+        #region Properties For Databinding
+
+        public string Tid
+        {
+            get { return _tid; }
+        }
+
+        #endregion
     }
 }
---
+++ BilletSystem_EksamensProjekt/tidspunktCatalog.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BilletSystem_EksamensProjekt
+{
+    public class TidspunktCatalog
+    {
+        #region Instance Field
+
+        private List<Tidspunkt> _tid;
+        private Tidspunkt _selectedTid;
+
+        #endregion
+
+        #region Constructor
+
+        public TidspunktCatalog()
+        {
+            _tid = new List<Tidspunkt>();
+
+            _tid.Add(new Tidspunkt("16:00"));
+            _tid.Add(new Tidspunkt("18:00"));
+            _tid.Add(new Tidspunkt("20:00"));
+
+            _selectedTid = _tid[0];
+        }
+
+        #endregion
+
+        #region Properties For Databinding
+
+        public List<Tidspunkt> Tid
+        {
+            get { return _tid; }
+        }
+
+        public Tidspunkt selectedTid
+        {
+            get { return _selectedTid; }
+            set
+            {
+                _selectedTid = value;
+                OnPropertyChanged();
+               
+            }
+        }
+        #endregion
+        #region INotifyPropertyChanged code
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
