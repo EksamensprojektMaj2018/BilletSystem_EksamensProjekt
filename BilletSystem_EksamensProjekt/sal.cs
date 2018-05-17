0cf4a6f37067b1664c1cf0e0b5d1c4fbb9f19f78
Hasan Al-Basri <hasa0173@edu.easj.dk>
2018-05-17 14:03:45 +0200

    opdateret på sal og salCatalog

--- BilletSystem_EksamensProjekt/sal.cs
+++ BilletSystem_EksamensProjekt/sal.cs
@@ -1,7 +1,47 @@
-﻿namespace BilletSystem_EksamensProjekt
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BilletSystem_EksamensProjekt
 {
     public class sal
     {
-        
+        #region Instance Field
+        private string _sale;
+        private string _noOfSeats;
+
+        #endregion
+
+        #region constructer
+
+        public sal(string sale, string noOfSeats)
+        {
+            _sale = sale;
+            _noOfSeats = noOfSeats;
+        }
+
+        #endregion
+
+
+        #region Properties for data binding
+
+        public string Sale
+        {
+            get { return _sale; }
+        }
+
+        public string NoOfSeats
+        {
+            get { return _noOfSeats; }
+        }
+        #endregion
+       
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
