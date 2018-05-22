5fd1aaa5192d3407e3db4afd3852160055ab214f
anilhusso <aklina17@gmail.com>
2018-05-22 12:10:43 +0200

    seats

---
+++ BilletSystem_EksamensProjekt/SeatCatalog.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BilletSystem_EksamensProjekt.Annotations;
+
+namespace BilletSystem_EksamensProjekt
+{
+    public class SeatCatalog : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- BilletSystem_EksamensProjekt/seats.cs
+++ BilletSystem_EksamensProjekt/seats.cs
@@ -1,7 +1,26 @@
-﻿namespace BilletSystem_EksamensProjekt
+﻿using Windows.ApplicationModel.Store.Preview.InstallControl;
+
+namespace BilletSystem_EksamensProjekt
 {
     public class seats
+    #region Instance fields
     {
-        
+        private string _Seats;
+    }
+#endregion
+    public Seats(string Seats)
+
+    #region Constructor
+    {
+    _Seats = _Seats;
     }
 }
+#endregion
+
+#region Properties for Databinding
+    public override string Tostring()
+{
+    
+}
+
+#endregion
