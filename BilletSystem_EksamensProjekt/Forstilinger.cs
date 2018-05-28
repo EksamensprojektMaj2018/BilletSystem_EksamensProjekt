79f02255b06b170954e273352677c4ab56c5707e
Lasse Henriksen <lass5676@edu.easj.dk>
2018-05-28 13:36:21 +0200

    Projektet "fuldendt"

---
+++ BilletSystem_EksamensProjekt/Forstilinger.cs
@@ -0,0 +1,31 @@
+﻿namespace BilletSystem_EksamensProjekt
+{
+    public class Forestillinger
+    {
+        private movies _movie;
+        private sal _sal;
+        private Tidspunkt _tidspunkt;
+
+        public movies Movies
+        {
+            get { return _movie; }
+        }
+
+        public sal Sal
+        {
+            get { return _sal; }
+        }
+
+        public Tidspunkt Tidspunkt
+        {
+            get { return _tidspunkt; }
+        }
+
+        public Forestillinger(movies movie, sal sale, Tidspunkt tid)
+        {
+            _movie = movie;
+            _sal = sale;
+            _tidspunkt = tid;
+        }
+    }
+}
---
+++ BilletSystem_EksamensProjekt/ForstilingerCatalog.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BilletSystem_EksamensProjekt
+{
+    public class ForestillingerCatalog : INotifyPropertyChanged
+    {
+        #region Instance Field
+
+        private moviesCatalog _movies;
+        private salCatalog _sale;
+        private TidspunktCatalog _tidspunkt;
+        private Forestillinger _selectedForestillinger;
+
+        private List<Forestillinger> _forestillinger;
+
+        #endregion
+
+        #region Constructor
+
+        public ForestillingerCatalog()
+        {
+            _forestillinger = new List<Forestillinger>();
+            _movies = new moviesCatalog();
+            _sale = new salCatalog();
+            _tidspunkt = new TidspunktCatalog();
+
+            _forestillinger.Add(new Forestillinger(_movies.Movies[0], _sale.sal[0], _tidspunkt.Tid[0]));
+            _forestillinger.Add(new Forestillinger(_movies.Movies[1], _sale.sal[0], _tidspunkt.Tid[1]));
+            _forestillinger.Add(new Forestillinger(_movies.Movies[2], _sale.sal[2], _tidspunkt.Tid[0]));
+            _selectedForestillinger = null;
+        }
+
+        #endregion
+
+        #region Properties For Databinding
+
+        public List<Forestillinger> Forestillinger
+        {
+            get { return _forestillinger; }
+        }
+
+        public Forestillinger SelectedForestillinger
+        {
+            get { return _selectedForestillinger; }
+            set
+            {
+                _selectedForestillinger = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+        #region INotifyPropertyChanged code
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
