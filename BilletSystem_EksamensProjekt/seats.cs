7fb4ab7a0ce4cf8cfe2c6037a6888a70befc4a3a
PowerTurtle <captainpowerturtle@gmail.com>
2018-05-28 02:03:13 +0200

    Tilføjede Forestillinger - Virker ikke

--- BilletSystem_EksamensProjekt/seats.cs
+++ BilletSystem_EksamensProjekt/seats.cs
@@ -2,25 +2,25 @@
 
 namespace BilletSystem_EksamensProjekt
 {
-    public class seats
-    #region Instance fields
-    {
-        private string _Seats;
-    }
-#endregion
-    public Seats(string Seats)
+//    public class seats
+//    #region Instance fields
+//    {
+//        private string _Seats;
+//    }
+//#endregion
+//    public Seats(string Seats)
 
-    #region Constructor
-    {
-    _Seats = _Seats;
-    }
-}
-#endregion
+//    #region Constructor
+//    {
+//    _Seats = _Seats;
+//    }
+//}
+//#endregion
 
-#region Properties for Databinding
-    public override string Tostring()
-{
+//#region Properties for Databinding
+//    public override string Tostring()
+//{
     
 }
 
-#endregion
+//#endregion
